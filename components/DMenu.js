8c856e8b29aa8cbb6559e99d92280664d953384d
Ruslan <kazarinskov.ruslan54@gmail.com>
2022-12-01 22:29:08 +0700

    menuV0.3

--- components/DMenu.js
+++ components/DMenu.js
@@ -1,11 +1,15 @@
-import { MenuItems } from "./MenuItems";
+import { LoginItems, MenuItems } from "./MenuItems";
 import styles from "/styles/Home.module.scss";
+import Divider from "@mui/material/Divider";
 
 export default function DisplayMenu() {
   return (
     <nav className={styles.dMenu}>
+      <LoginItems />
+      <Divider />
       <MenuItems name="Home" />
       <MenuItems name="Communities" />
+      <Divider />
     </nav>
   );
 }
--- components/MenuItems.js
+++ components/MenuItems.js
@@ -1,5 +1,17 @@
 import MenuItem from "@mui/material/MenuItem";
 import styles from "/styles/Home.module.scss";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    red: {
+      main: "#c8000d",
+      contrastText: "#fff",
+    },
+  },
+});
 
 export function MenuItems({ name, icon = "/menu/" + name + ".svg" }) {
   return (
@@ -11,3 +23,29 @@ export function MenuItems({ name, icon = "/menu/" + name + ".svg" }) {
     </>
   );
 }
+
+export function LoginItems({ avatar, name, profession }) {
+  return (
+    <div className={styles.loginItems}>
+      <img className={styles.loginItemsAva} src={avatar} alt="avatar" />
+      <div className={styles.loginItemsName}>{name}</div>
+      <div className={styles.loginItemsProfession}>{profession}</div>
+      <ThemeProvider theme={theme}>
+        <Button
+          className={styles.loginItemsButton}
+          color="red"
+          size="large"
+          variant="contained"
+          startIcon={<AddIcon />}
+        >
+          CREATE
+        </Button>
+      </ThemeProvider>
+    </div>
+  );
+}
+LoginItems.defaultProps = {
+  avatar: "/menu/ava.png",
+  name: "Mr Usman Jaffer",
+  profession: "Vascular Surgeon",
+};
