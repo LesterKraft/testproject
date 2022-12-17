47432656330de9c4fa5571a9f7ac2620cb7cb32d
Ruslan <kazarinskov.ruslan54@gmail.com>
2022-12-17 13:03:47 +0700

    CardV1

--- components/CardAnswer.js
+++ components/CardAnswer.js
@@ -2,23 +2,19 @@ import { Card, Divider, IconButton } from "@mui/material";
 import styles from "/styles/Home.module.scss";
 import CardHeader from "@mui/material/CardHeader";
 import Collapse from "@mui/material/Collapse";
-import { useState } from "react";
 import * as React from "react";
-import CardContent from "@mui/material/CardContent";
-
-// import Avatar from "@mui/material/Avatar";
-// import FileUploadIcon from "@mui/icons-material/FileUpload";
 import Button from "@mui/material/Button";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-// import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-// import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
-// import IconButton from "@mui/material/IconButton";
-// import ForwardIcon from "@mui/icons-material/Forward";
-// import ShareIcon from "@mui/icons-material/Share";
-// import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { styled } from "@mui/material/styles";
 import QuestionAnswerIcon from "@mui/icons-material/QuestionAnswer";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import EditIcon from "@mui/icons-material/Edit";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import ForwardIcon from "@mui/icons-material/Forward";
+import ShareIcon from "@mui/icons-material/Share";
 
 const theme = createTheme({
   palette: {
@@ -64,9 +60,6 @@ export default function CardAnswer() {
             >
               1 Answer
             </Button>
-            {/* <IconButton>
-              <ExpandMoreIcon />
-            </IconButton> */}
 
             <ExpandMore
               expand={expanded}
@@ -77,23 +70,83 @@ export default function CardAnswer() {
               <ExpandMoreIcon />
             </ExpandMore>
           </div>
-          <Divider className={styles.answerDivider} />
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
+
+          <Collapse
+            className={styles.answerBox}
+            in={expanded}
+            timeout="auto"
+            unmountOnExit
+          >
+            <Divider className={styles.answerDivider} />
+            <div className={styles.answer}>
               <CardHeader
-                className={styles.cardHeader}
+                className={styles.answerHeader}
                 avatar={
                   <img
-                    className={styles.cardHeaderAva}
+                    className={styles.answerHeaderAva}
                     src="/menu/ava.png"
                     alt="ava"
                   />
                 }
-                action={<IconButton aria-label="settings"></IconButton>}
-                title="Mr Usman Jaffer"
-                subheader="Vascular Surgeon"
+                action={
+                  <Button
+                    className={styles.answerHeaderEdit}
+                    color="red"
+                    size="large"
+                    aria-label="edit"
+                    startIcon={
+                      <EditIcon className={styles.answerHeaderEditIcon} />
+                    }
+                  >
+                    Edit
+                  </Button>
+                }
+                title={
+                  <div className={styles.answerHeaderTitle}>
+                    Mr Usman Jaffer
+                  </div>
+                }
+                subheader={
+                  <div className={styles.answerHeaderSubheader}>
+                    Vascular Surgeon
+                  </div>
+                }
               />
-            </CardContent>
+              <div className={styles.answerText}>
+                If you find yourself dealing with painful varicose veins,
+                cramping in your legs, swelling, or restless legs, chances are
+                good EVLA is right for you. EVLA focuses on veins that are
+                larger and deeper in your legs — the smaller, more surface once
+                are better suited for sclerotherapy. Your vascular specialist
+                will be sure to come up with a treatment plan that caters to
+                your specific needs, not a generalized one-size-fits-all plan.
+              </div>
+              <div className={styles.answerView}>1 view • Now</div>
+              <div className={styles.cardFooter}>
+                <div>
+                  <Button
+                    className={styles.cardFooterUploads}
+                    color="red"
+                    size="large"
+                    aria-label="upvotes"
+                    startIcon={<FileUploadIcon />}
+                  >
+                    Upvote
+                  </Button>
+                </div>
+                <div>
+                  <IconButton aria-label="down" size="large">
+                    <ForwardIcon />
+                  </IconButton>
+                  <IconButton aria-label="share" size="large">
+                    <ShareIcon />
+                  </IconButton>
+                  <IconButton aria-label="more" size="large">
+                    <MoreHorizIcon />
+                  </IconButton>
+                </div>
+              </div>
+            </div>
           </Collapse>
         </Card>
       </ThemeProvider>
