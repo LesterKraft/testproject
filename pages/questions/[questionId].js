5b06cf9d5f736fb61606628f2c0d023c504f22ab
Ruslan <kazarinskov.ruslan54@gmail.com>
2022-12-29 00:45:10 +0700

    question props read

--- pages/questions/[questionId].js
+++ pages/questions/[questionId].js
@@ -7,33 +7,6 @@ import Cards from "../../components/card/Cards";
 import { doc, getDoc } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 
-export default function Post(props) {
-  console.log("props", props);
-  const router = useRouter();
-  const question = {
-    title: "How do I know if EVLA is right for me?",
-    description:
-      "I'm 30Y female with spider vein problems since age 18. After years of sclero, my doctor recommended ultrasound scans. I was then diagnosed me with advanced venous insufficiency and recommended EVLT. From what I've read EVLT is almost exclusively used to treat varicose veins. I do not have varicose veins (yet!) but am interested in warding off future problems as well as treating my current spider veins. Is EVLT a good candidate for someone with advanced venous insufficiency but no varicose veins?",
-    tags: ["ELVA", "Agorafobia"],
-    views: 6,
-    timestamp: new Date(1057165200000),
-  };
-
-  return (
-    <>
-      <CardQuestion question={question} />
-      <CardAnswer />
-      <div className={styles.line}>
-        <div className={styles.lineText}>Realted Questions</div>
-        <Link href="/pages/questions.js" className={styles.lineMore}>
-          SEE MORE
-        </Link>
-      </div>
-      <Cards />
-    </>
-  );
-}
-
 export async function getServerSideProps(context) {
   const db = getFirestore();
   const docRef = doc(db, "question", context.query.questionId);
@@ -53,4 +26,31 @@ export async function getServerSideProps(context) {
     // doc.data() will be undefined in this case
     console.log("No such document!");
   }
+}
+
+export default function Post(props) {
+  console.log("props", props);
+  const router = useRouter();
+  const question = {
+    title: props.question.title,
+    description: props.question.description,
+
+    tags: props.question.tags,
+    views: 6,
+    timestamp: new Date(1057165200000),
+  };
+
+  return (
+    <>
+      <CardQuestion question={question} />
+      <CardAnswer />
+      <div className={styles.line}>
+        <div className={styles.lineText}>Realted Questions</div>
+        <Link href="/pages/questions.js" className={styles.lineMore}>
+          SEE MORE
+        </Link>
+      </div>
+      <Cards />
+    </>
+  );
 }
